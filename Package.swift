aa7406fcf8b50bb0789ef6bb434f01bf6560ef65
Yevhenii Kalashnikov <lumyk@me.com>
2024-04-17 14:26:05 +0100

    Updated Surge version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // Here, 'Surge' is used for matrix and vector computations, a common requirement for implementing Kalman filters.
-        .package(url: "https://github.com/Jounce/Surge.git", from: "2.0.0")
+        .package(url: "https://github.com/Jounce/Surge.git", from: "2.3.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
